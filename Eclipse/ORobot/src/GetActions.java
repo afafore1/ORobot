d2c33474f2fea1841bf84ece71c3e821072e012e
ayo <faforeayomitunde@yahoo.com>
2015-09-13 11:38:28 -0700

    Adding new class that will be used to get user actions and store it in a text file named commands.txt in desktop

---
+++ Eclipse/ORobot/src/GetActions.java
@@ -0,0 +1,11 @@
+/*
+ * This class will be used to get user actions and store them in a text file which will be used later 
+ */
+public class GetActions {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
